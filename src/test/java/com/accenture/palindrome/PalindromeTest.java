366cf8d30b3e926e760ff3ec5c7aad1a487f7a1f
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2020-09-02 10:24:10 -0400

    Verifying empty string doesn't cause an explosion

--- src/test/java/com/accenture/palindrome/PalindromeTest.java
+++ src/test/java/com/accenture/palindrome/PalindromeTest.java
@@ -7,13 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PalindromeTest {
 
     @Test
-    public void a_becomes_aa() {
+    void a_becomes_aa() {
         String palindrome = Palindrome.from("a");
         assertThat(palindrome).isEqualTo("aa");
     }
 
     @Test
-    public void double_and_reverse_string() {
+    void double_and_reverse_string() {
         String palindrome = Palindrome.from("12345");
         assertThat(palindrome).isEqualTo("5432112345");
     }
@@ -22,5 +22,11 @@ public class PalindromeTest {
     void existing_palindromes_remain_the_same() {
         String palindrome = Palindrome.from("cc");
         assertThat(palindrome).isEqualTo("cc");
+    }
+
+    @Test
+    void empty_string_remains_empty_string() {
+        String palindrome = Palindrome.from("");
+        assertThat(palindrome).isEqualTo("");
     }
 }
